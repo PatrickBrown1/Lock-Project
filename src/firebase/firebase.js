6710b6009b5c4b078d4ae47841636f06feebcb0e
Patrick Brown <patrickbrown1312@gmail.com>
2018-07-30 17:30:37 -0700

    added some firebase integration, ended on Sign Up with React and Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAeryMZkx937sD6vgp0kb_2qvWEz0hZbPI",
+    authDomain: "lock-app-dbba5.firebaseapp.com",
+    databaseURL: "https://lock-app-dbba5.firebaseio.com",
+    projectId: "lock-app-dbba5",
+    storageBucket: "lock-app-dbba5.appspot.com",
+    messagingSenderId: "468420612945"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};
